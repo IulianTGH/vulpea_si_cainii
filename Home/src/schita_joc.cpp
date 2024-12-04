4d0a1221adab56fef160ab008317e20ca3e694f8
IulianTGH <jorden.pascal@proton.me>
2024-12-04 23:59:42 +0200

    conditie victorie caini

--- Home/src/schita_joc.cpp
+++ Home/src/schita_joc.cpp
@@ -3,16 +3,45 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAX 8
-
 int left, top, size, x, y, xv, yv, latura, numar, linie, coloana, old_line, old_column, win=0;
 
-int TablaDeJoc[MAX][MAX] = {0};
+int TablaDeJoc[8][8] = {0};
+
+int mutariPosibile(int line, int column) {
+    int mutari = 0;
+    // Verifică diagonală sus-stânga
+    if (line > 1 && column > 1 && TablaDeJoc[line - 1][column - 1] == 0)    mutari++;
+    // Verifică diagonală sus-dreapta
+    if (line > 1 && column < 8 && TablaDeJoc[line - 1][column + 1] == 0)    mutari++;
+    // Verifică diagonală jos-stânga
+    if (line < 8 && column > 1 && TablaDeJoc[line + 1][column - 1] == 0)    mutari++;
+    // Verifică diagonală jos-dreapta
+    if (line < 8 && column < 8 && TablaDeJoc[line + 1][column + 1] == 0)    mutari++;
+    return mutari;
+}
+
 
 void gameFinish() {
-    for(int i=1; i<=numar; i++)
-    if(TablaDeJoc[numar][i]==1) win=1;
+    // Verifică dacă vulpea a ajuns pe ultima linie
+    for (int i = 1; i <= numar; i++) {
+        if (TablaDeJoc[numar][i] == 1) { // Linia 8
+            win = 1; // Vulpea a câștigat
+            return;
+        }
+    }
+    // Verifică dacă vulpea mai are mutări posibile
+    for (int i = 1; i <= numar; i++)
+        for (int j = 1; j <= numar; j++)
+            if (TablaDeJoc[i][j] == 1) { // Găsește poziția vulpii
+                int line = i;
+                int column = j;
+                if (mutariPosibile(line,column) > 0)
+                    return; // Vulpea mai are mutări, jocul continuă
+            }
+    // Dacă nu mai are mutări, câinii au câștigat
+    win = 2;
 }
+
 
 void drawPiece(int center_x, int center_y, int culoare) {
     setfillstyle(SOLID_FILL, culoare);
@@ -58,7 +87,6 @@ void schimbarePozitie(int *player, int old_line, int old_column) {
 }
 
 void mutaPiesa(int* player) {
-    gameFinish();
     char p[20]; sprintf(p,"Jucator %d",*player);
     outtextxy(380,20,p);
     if(ismouseclick(WM_LBUTTONDOWN)) {
@@ -72,6 +100,7 @@ void mutaPiesa(int* player) {
                 clearmouseclick(WM_LBUTTONDOWN);
                 x = mousex(); y = mousey();
                 schimbarePozitie(player,old_line,old_column);
+                gameFinish();
                 break;
                 }
             }
@@ -96,9 +125,9 @@ void desenTabla() {
 }
 
 void afisareMatrice() {
-    for (int i=1; i<=numar; i++) {
-    for (int j=1; j<=numar; j++) {
-        printf("%d\t",TablaDeJoc[i][j]);
+    for (int i=1; i<=8; i++) {
+    for (int j=1; j<=8; j++) {
+        printf("%d ",TablaDeJoc[i][j]);
     }
     printf("\n");
     }
@@ -131,24 +160,28 @@ void debugInfo() {
 }
 
 int main() {
-    initwindow(800,600,"Program mouse rudimentar");
+    initwindow(800, 600, "Program mouse rudimentar");
     desenTabla();
     seteazaTabla();
     int jucator = 1;
-    //commit test
-    while (1)
-    {
+    while (1) {
         int ok=0;
-        //debugInfo(); // modul pentru depanare
-        //OLD_punePiesa(&jucator);
-        if(!win) mutaPiesa(&jucator);
-        if(kbhit()) {
-            char key = getch();
-            if(key==27) break;
+        // Verifică dacă jocul s-a terminat
+        gameFinish();
+        // Dacă cineva a câștigat, afișează mesajul și ieși din buclă
+        if (win==1&&ok==0) {
+            outtextxy(380, 20, "Vulpea a castigat!");
+            ok=1;
         }
-        if(win==1&&ok==0) {
-        outtextxy(380,20,"Vulpea a castigat!");
-        ok=1;
+        if (win==2&&ok==0) {
+            outtextxy(380, 20, "Cainii au castigat!");
+            ok=1;
+        }
+        if(!win)    mutaPiesa(&jucator);
+        // Verifică input de la tastatură pentru a ieși
+        if (kbhit()) {
+            char key = getch();
+            if (key == 27) break; // Apasă ESC pentru a ieși
         }
         delay(50);
     }
